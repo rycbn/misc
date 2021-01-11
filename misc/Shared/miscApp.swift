ed5f61dd5830bc211731cc14ab20890c878dd164
Chau Boon Yong <rycbn@hotmail.com>
2021-01-11 20:34:14 +0000

    add xcode project

---
+++ misc/Shared/miscApp.swift
@@ -0,0 +1,17 @@
+//
+//  miscApp.swift
+//  misc
+//
+//  Created by Chau Boon Yong on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct miscApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
